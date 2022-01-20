50555db2f048dc35355f6f9fb9a6c66c5faf340c
TSION2121 <tsionbizuayehu3@gmail.com>
2022-01-20 21:13:17 +0300

    activity5

---
+++ App/activity5/actvity3/resource/js/script.js
@@ -0,0 +1,20 @@
+function sayWelcome() {
+    alert("Welcome visitor")
+ }
+ function over() {
+    alert("Mouse Over");
+ }            
+ function out() {
+    document.write("Subm");}
+function myFunction(val) {
+        document.getElementById("demo").innerHTML = val; 
+      }
+function myFunction1() {
+let person = prompt("Please enter your name");
+if (person != null) {
+document.getElementById("name").innerHTML = "Hello " + person + "welcome to ";
+        }
+      }
+function submitReserve() {
+        confirm("Press a button!");
+      }
